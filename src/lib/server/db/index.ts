c2d3e27e3796bb01684f95fad27b22d81a81eec4
Sain-Biswas <sain.biswas614@gmail.com>
2025-02-17 21:52:58 +0530

    Add Drizzle schema and .db from pokemon data fetcher

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,6 +1,12 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
+
+import schema from './schema/index.schema';
+
 import { env } from '$env/dynamic/private';
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+
 const client = createClient({ url: env.DATABASE_URL });
-export const db = drizzle(client);
+const db = drizzle(client, { schema });
+
+export default db;
